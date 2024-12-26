d4dc9afad781d33a30c5ddc9a0c7d8622e2769ed
Mini9709 <tjdals9709@naver.com>
2024-12-26 17:56:17 +0900

    feat : enum 클래스 추가

--- src/main/java/com/example/trelloproject/user/enumclass/UserRole.java
+++ src/main/java/com/example/trelloproject/user/enumclass/UserRole.java
@@ -1,5 +1,31 @@
 package com.example.trelloproject.user.enumclass;
 
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+@Getter
 public enum UserRole {
-    USER, ADMIN
+    USER("USER"), ADMIN("ADMIN");
+
+    private final String name;
+
+    UserRole(String name) {
+        this.name = name;
+    }
+
+    public static UserRole of(String name) throws IllegalArgumentException {
+        for (UserRole role : values()) {
+            if (role.getName().equals(name)) {
+                return role;
+            }
+        }
+
+        throw new IllegalArgumentException("해당하는 권한이 존재하지 않습니다." + name);
+    }
+
+    public List<SimpleGrantedAuthority> getAuthority() {
+        return List.of(new SimpleGrantedAuthority(name));
+    }
 }
