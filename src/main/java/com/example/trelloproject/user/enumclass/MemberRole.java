3b328e4383720234b6bace9df0d075f6bc9b3a8b
Mini9709 <tjdals9709@naver.com>
2024-12-27 17:41:34 +0900

    feat : enum 관련 메서드 추가

--- src/main/java/com/example/trelloproject/user/enumclass/MemberRole.java
+++ src/main/java/com/example/trelloproject/user/enumclass/MemberRole.java
@@ -1,5 +1,24 @@
 package com.example.trelloproject.user.enumclass;
 
+import lombok.Getter;
+
+@Getter
 public enum MemberRole {
-    WSADMIN, MEMBER, ONLY_READ
+    WSADMIN("WSADMIN"), MEMBER("MEMBER"), ONLYREAD("ONLYREAD");
+
+    private String name;
+
+    MemberRole(String name) {
+        this.name = name;
+    }
+
+    public static MemberRole of(String name) throws IllegalArgumentException {
+        for (MemberRole role : values()) {
+            if (role.getName().equals(name)) {
+                return role;
+            }
+        }
+
+        throw new IllegalArgumentException("해당하는 권한이 존재하지 않습니다." + name);
+    }
 }
