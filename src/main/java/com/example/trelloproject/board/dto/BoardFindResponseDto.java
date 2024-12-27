5dbecff8974ca8990907cce9ea330c5c0e4ca036
dbtmdgml98 <dbtmdgml98@naver.com>
2024-12-27 13:11:18 +0900

    feat: Board 단건 조회 기능 구현 추가
    리스트 목록 추가

--- src/main/java/com/example/trelloproject/board/dto/BoardFindResponseDto.java
+++ src/main/java/com/example/trelloproject/board/dto/BoardFindResponseDto.java
@@ -1,12 +1,14 @@
 package com.example.trelloproject.board.dto;
 
 import com.example.trelloproject.board.entity.Board;
-import com.example.trelloproject.list.entity.BoardList;
+import com.example.trelloproject.list.dto.ListCreateResponseDto;
 import com.example.trelloproject.workspace.entity.Workspace;
 import lombok.Getter;
 
 import java.io.File;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class BoardFindResponseDto {
@@ -15,16 +17,17 @@ public class BoardFindResponseDto {
     private final Long workspaceId;
     private final String title;
     private final File image;
-    //    private final List<BoardListResponseDto> boards;
+    private final List<ListCreateResponseDto> lists;
     private final LocalDateTime createdAt;
     private final LocalDateTime updateAt;
 
-    public BoardFindResponseDto(Long boardId, Long workspaceId, String title, File image, LocalDateTime createdAt, LocalDateTime updateAt) {
+    public BoardFindResponseDto(Long boardId, Long workspaceId, String title, File image, List<ListCreateResponseDto> lists, LocalDateTime createdAt, LocalDateTime updateAt) {
 
         this.boardId = boardId;
         this.workspaceId = workspaceId;
         this.title = title;
         this.image = image;
+        this.lists = lists;
         this.createdAt = createdAt;
         this.updateAt = updateAt;
     }
@@ -36,6 +39,9 @@ public class BoardFindResponseDto {
                 workspace.getWorkspaceId(),
                 board.getTitle(),
                 board.getImage(),
+                board.getBoardLists().stream()
+                        .map(ListCreateResponseDto::toDto)
+                        .collect(Collectors.toList()),
                 board.getCreatedAt(),
                 board.getUpdatedAt()
         );
