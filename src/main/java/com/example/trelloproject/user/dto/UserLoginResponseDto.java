1fb0460a7f7cd2275d184ce34baf36d1b1ad1756
Mini9709 <tjdals9709@naver.com>
2024-12-29 18:40:29 +0900

    feat : 로그인 dto 수정

--- src/main/java/com/example/trelloproject/user/dto/UserLoginResponseDto.java
+++ src/main/java/com/example/trelloproject/user/dto/UserLoginResponseDto.java
@@ -4,11 +4,15 @@ import lombok.Getter;
 
 @Getter
 public class UserLoginResponseDto {
+    private String message;
     private String name;
     private String email;
+    private String accessToken;
 
-    public UserLoginResponseDto(String name, String email) {
+    public UserLoginResponseDto(String message, String name, String email, String accessToken) {
+        this.message = message;
         this.name = name;
         this.email = email;
+        this.accessToken = accessToken;
     }
 }
